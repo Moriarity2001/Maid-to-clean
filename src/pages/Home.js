1f8bd982355254253c97764bab3818b5a8436cbd
maniac-morty <himanshu.semwal@spyne.ai>
2025-03-24 20:09:13 +0530

    added data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,5 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
-import PromoSection from "../components/PromoSection";
-import AwardsSection from "../components/AwardsSection";
-import FeaturedPage from "../components/FeaturedPage";
-import CleaningServices from "../components/CleaningServices";
-import Services from "../components/Services";
-import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
